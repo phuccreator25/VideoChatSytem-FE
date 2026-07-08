36d7da06dd22999d8cb903537c24d1bf1b68774c
phuccreator25 <nguyentruongphuc.25022004@gmail.com>
2026-07-08 16:02:19 +0700

    FIX: config API HOST so that connect BE(deploying)

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   // API_HOST: "http://localhost:3000/api",
-  APP_HOST: "http://localhost:5173",
-  // APP_HOST: 'http://127.0.0.1:30080',
+  // APP_HOST: "http://localhost:5173",
+  APP_HOST: "https://videochatsystem-fe.vercel.app",
   API_HOST: "https://videochatsytem-be.onrender.com/api",
   MODE: "development",
   VITE_GIPHY_API_KEY: "krTuAwfxoHwAivfT0U7QQgSY2vQ4wfhG",
