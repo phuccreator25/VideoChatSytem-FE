03e75df2e7c0ce2f6dca66f4134434df8f36e93f
phuccreator25 <nguyentruongphuc.25022004@gmail.com>
2026-07-08 15:57:48 +0700

    FIX: config API HOST so that connect BE(deploying)

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -2,7 +2,7 @@ export const CONFIG = {
   // API_HOST: "http://localhost:3000/api",
   APP_HOST: "http://localhost:5173",
   // APP_HOST: 'http://127.0.0.1:30080',
-  API_HOST: "https://videochatsytem-be.onrender.com",
+  API_HOST: "https://videochatsytem-be.onrender.com/api",
   MODE: "development",
   VITE_GIPHY_API_KEY: "krTuAwfxoHwAivfT0U7QQgSY2vQ4wfhG",
 };
