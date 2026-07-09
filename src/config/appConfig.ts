871f309f4f02332264d46f3670ddfa49bcd311ba
phuccreator25 <nguyentruongphuc.25022004@gmail.com>
2026-07-09 09:47:19 +0700

    fix: app config change domain

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,8 +1,8 @@
 export const CONFIG = {
-  API_HOST: "http://localhost:3000/api",
-  APP_HOST: "http://localhost:5173",
-  // APP_HOST: "https://videochatsystem-fe.vercel.app",
-  // API_HOST: "https://videochatsytem-be.onrender.com/api",
+  // API_HOST: "http://localhost:3000/api",
+  // APP_HOST: "http://localhost:5173",
+  APP_HOST: "https://videochatsystem-fe.vercel.app",
+  API_HOST: "https://videochatsytem-be.onrender.com/api",
   MODE: "development",
   VITE_GIPHY_API_KEY: "krTuAwfxoHwAivfT0U7QQgSY2vQ4wfhG",
 };
