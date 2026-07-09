1857860df5fbabd717cf3bd58bb6aa87243bd5ec
phuccreator25 <nguyentruongphuc.25022004@gmail.com>
2026-07-09 10:29:45 +0700

    Refactor: refactor UI reponse

--- src/components/VideoCall/dialogVideoCall.chat.tsx
+++ src/components/VideoCall/dialogVideoCall.chat.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useVideoCall } from "../../hooks/Call/videoCall.hook";
 import {
     Dialog,
