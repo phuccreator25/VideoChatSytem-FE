60a01089466f6c9a3a53d188cc45671837bc3745
Nguyễn Trường Phúc <nguyentruongphuc.25022004@gmail.com>
2026-08-06 11:36:07 +0700

    Modify appConfig for local development settings
    Updated API and APP host configurations for local development.

--- src/config/appConfig.ts
+++ src/config/appConfig.ts
@@ -1,10 +1,10 @@
 export const CONFIG = {
-  API_HOST: "http://localhost:3000/api",
-  APP_HOST: "http://localhost:5173",
+  // API_HOST: "http://localhost:3000/api",
+  // APP_HOST: "http://localhost:5173",
   // APP_HOST: "https://videochatsystem-fe.vercel.app",
   // API_HOST: "https://videochatsytem-be.onrender.com/api",
-  // APP_HOST: "http://chat-app.test",
-  // API_HOST: "http://chat-app.test/api",
+  APP_HOST: "http://localhost:30081",
+  API_HOST: "http://localhost:30001/api",
   MODE: "development",
   VITE_GIPHY_API_KEY: "krTuAwfxoHwAivfT0U7QQgSY2vQ4wfhG",
 };
